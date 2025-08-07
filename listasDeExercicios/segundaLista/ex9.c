c908d9e2316be39f49c52300c69345b4cd6f4f50
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-07 19:53:37 -0300

    Iniciando o ex9

--- listasDeExercicios/segundaLista/ex9.c
+++ listasDeExercicios/segundaLista/ex9.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
 int main(){
+    printf("\n===Eleicao Sindicalista===\n");
+
+    int votosTotal, votosNulos, votosBranco, votosACandidato, votosBCandidato, votosCCandidato;
     
+    float perceitualTotal, percentualA, percentualB, percentualC, percentualVotosBrancos;
+
+    printf("\nDigite quantos votos o candidato A teve: \n");
+    scanf("%d", &votosACandidato);
+
+    printf("\nDigite quantos votos o candidato A teve: \n");
+    scanf("%d", &votosBCandidato);
+
+    printf("\nDigite quantos votos o candidato A teve: \n");
+    scanf("%d", &votosCCandidato);
 
     return 0;
 }
