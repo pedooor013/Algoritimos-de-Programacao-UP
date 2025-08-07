52e518149e84cb0bc17ab93396560e56e65b9752
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-07 07:58:26 -0300

    Finalização do ex8

--- listasDeExercicios/segundaLista/ex8.c
+++ listasDeExercicios/segundaLista/ex8.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
 int main(){
+    printf("\n===Calculo novo salario===\n");
+
+    float salarioAtual, salarioNovo, porcentagemAumento;
+
+    printf("Digite o valor do seu salario atual: ");
+    scanf("%f", &salarioAtual);
     
+    printf("Digite a porcentagem de aumento salarial: ");
+    scanf("%f", &porcentagemAumento);
+
+    salarioNovo = ((porcentagemAumento/100)*salarioAtual)+salarioAtual;
+
+    printf("O seu novo salario eh de R$%.2f", salarioNovo);
+
 
     return 0;
 }
