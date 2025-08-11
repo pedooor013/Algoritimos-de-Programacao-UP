fc8a3aea5cc7bea82bd234125acc6f240bdc1e4d
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-11 07:57:11 -0300

    Finalização da lista de exercicios e do exercicio 10

--- algoritimosDeProgramacao/listasDeExercicios/segundaLista/ex10.c
+++ algoritimosDeProgramacao/listasDeExercicios/segundaLista/ex10.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
-int main(){
-    
+int main()
+{
 
+    float num1, num2, resultSoma, resultMutiplicacao, resultDivicao, resultSubtracao;
+
+    printf("\n===Operacoes Basicas===\n");
+
+    printf("\nDigite um número real: \n");
+    scanf("%f", &num1);
+
+    printf("\nDigite um número real: \n");
+    scanf("%f", &num2);
+
+    resultSoma = num1 + num2;
+    resultSubtracao = num1 - num2;
+    resultMutiplicacao = num1 * num2;
+    resultDivicao = num1 / num2;
+
+    printf("\n%.2f + %2.f = %.2f \n %.2f - %.2f = %.2f \n %.2f * %.2f = %.2f \n %.2f / %.2f = %.2f", num1, num2, resultSoma, num1, num2, resultSubtracao, num1, num2, resultMutiplicacao, num1, num2, resultDivicao);
     return 0;
 }
