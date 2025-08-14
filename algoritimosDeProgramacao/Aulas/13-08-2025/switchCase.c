4ca2eec74650890e1fda4a523f0db51ee4ce7adc
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-13 21:27:38 -0300

    addd: Conteudo de switch case em C

--- algoritimosDeProgramacao/Aulas/13-08-2025/switchCase.c
+++ algoritimosDeProgramacao/Aulas/13-08-2025/switchCase.c
@@ -1,23 +1,52 @@
 #include <stdio.h>
 
-int main(){
-/* 
-    Codigo para representar como deve ser feito um switch case em C 
-*/
-    int condicaoSwitch;
+int main()
+{
+    /*
+        Codigo para representar como deve ser feito um switch case em C
+    */
+    int escolhaUsuario, numEscolhidoUsuario, segundoNumUser, quadradoNumUser, senhaUser;
+    int senha = 1308;
+    printf("\nDescubra a senha numerica para avancar: \nSenha: \n");
+    scanf("%d", &senhaUser);
 
-    scanf("%d", &condicaoSwitch);
-    switch (condicaoSwitch){
-    case 1:
-        printf("\nDeu 1\n");
-        break;
-    case 2:
-        printf("\nDeu 2\n");
-        break;
-    default:
-        printf("\nNao deu 1 nem 2\n");
-        break;
+    if (senhaUser == senha)
+    {
+        printf("\n\n===Voce descobriu a senha===\n\n");
+        printf("\nDigite um numero: \n");
+        scanf("%d", &numEscolhidoUsuario);
+
+        printf("\nEscolha uma operacao para fazer com o numero escolhido: \n01) Verificar se eh par ou impar\n02) Encontrar o quadrado do numero\n03)Divisao entre 2 numeros\n\nResposta usuario: \n");
+        scanf("%d", &escolhaUsuario);
+
+        switch (escolhaUsuario)
+        {
+        case 1:
+            if (numEscolhidoUsuario % 2 == 0)
+            {
+                printf("\n%d eh par!\n", numEscolhidoUsuario);
+            }
+            else
+            {
+                printf("\n%d eh impar!\n", numEscolhidoUsuario);
+            }
+            break;
+        case 2:
+            quadradoNumUser = numEscolhidoUsuario * numEscolhidoUsuario;
+            printf("\nO quadrado de %d eh %d\n", numEscolhidoUsuario, quadradoNumUser);
+            break;
+        case 3:
+            printf("\nDigite outro numero: \n");
+            scanf("%d", &segundoNumUser);
+            float resultOperacao = (float)numEscolhidoUsuario / segundoNumUser;
+            printf("\n%d / %d = %.2f\n", numEscolhidoUsuario, segundoNumUser, resultOperacao);
+            break;
+        default:
+            printf("\nDigite um valor valido...\n");
+        }
+    }else{
+        printf("Tente novamente...");
+        main();
     }
-
     return 0;
 }
