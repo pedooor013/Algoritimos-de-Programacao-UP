dedc07b7bc5ddfc2bc747e652695df81c8ba539a
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-20 21:04:29 -0300

    addd: Resolução do exercicios 13

--- algoritimosDeProgramacao/listasDeExercicios/terceiraLista/ex13.c
+++ algoritimosDeProgramacao/listasDeExercicios/terceiraLista/ex13.c
@@ -2,7 +2,46 @@
 
 int main(){
 
-    
+    printf("===Calculo de Natalidade===");
 
+    int numAbitantes, escolhaUser, numNascimento, numObitos;
+    float result;
+
+    printf("\nSelecione:\n01) Taxa de Natalidade\n02) Taxa de Obito\nR:");
+    scanf("%d", &escolhaUser);
+
+    switch (escolhaUser)
+    {
+    case 1:
+
+        printf("\nDigite a quantidade de nascimentos: \n");
+        scanf("%d", &numNascimento);
+        
+        printf("\nDigite a quantidade de abitantes: \n");
+        scanf("%d", &numAbitantes);
+        
+        
+        result = (float)(numNascimento*1000)/numAbitantes;
+        
+        printf("A taxa de natalidade eh igual a %.2f", result);
+        
+        break;
+        case 2:
+        
+        printf("\nDigite a quantidade de obitos: \n");
+        scanf("%d", &numObitos);
+        
+        printf("\nDigite a quantidade de abitantes: \n");
+        scanf("%d", &numAbitantes);
+
+        result = (float)(numObitos*1000)/numAbitantes;
+        
+        printf("A taxa de obitos eh igual a %.2f", result);
+        
+        break;
+    default:
+        printf("\nDigite um numero valido...\n");
+        break;
+    }
     return 0;
 }
