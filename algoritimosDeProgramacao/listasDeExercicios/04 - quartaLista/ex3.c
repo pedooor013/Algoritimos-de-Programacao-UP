828fcb3a122708ae6c9c7878f5645678606abdae
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-25 21:31:36 -0300

    addd: resoluçã ex3

--- algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex3.c
+++ algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex3.c
@@ -2,5 +2,14 @@
 
 int main(){
 
+    int result;
+
+    result = 0;
+
+    for(int i = 0; i <=100; i++){
+        result = result+i;
+    }
+    printf("\nA soma de 0 ate 100 eh igual a %d\n", result);
+
     return 0;
 }
