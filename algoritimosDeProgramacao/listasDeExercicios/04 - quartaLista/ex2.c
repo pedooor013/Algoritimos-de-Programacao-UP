51bf051622b4588bce86f8271a84f1b48330b438
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-25 21:28:33 -0300

    addd: resolução do ex2

--- algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex2.c
+++ algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex2.c
@@ -2,5 +2,15 @@
 
 int main(){
 
+    int numUsuario;
+
+    printf("\nDigite um numero: \n");
+    scanf("%d", &numUsuario);
+
+    for(int i = 0; i <= 10 ; i++){
+        printf("\n%d x %d = %d\n", numUsuario, i, (numUsuario*i));
+    
+    }
+
     return 0;
 }
