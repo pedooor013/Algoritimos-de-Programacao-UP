ba8d7bf7e255e3a6abe700cfa2f1dd2434eb3bd4
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-25 21:45:59 -0300

    addd: Conteudo da aula do dia 25/08/2025

--- algoritimosDeProgramacao/Aulas/25-08-2025/doWhile.c
+++ algoritimosDeProgramacao/Aulas/25-08-2025/doWhile.c
@@ -7,11 +7,17 @@ int main(){
     do{
         printf("\nDigite um numero: \n");
         scanf("%d", &numUsuario);
+        
         if(numUsuario<50 || numUsuario >150 ){
             printf("\nValor invalido...\n");
             numUsuario = 1;
         }
 
+        if(numUsuario % 2 == 0){
+            printf("\nO numero eh par!\n");
+        }if(numUsuario % 2 != 0){
+            printf("\nO numero eh impar!\n");
+        }
         result = numUsuario%5; 
 
 
