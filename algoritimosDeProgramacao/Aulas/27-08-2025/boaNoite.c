c52e05f38e2f13142f173a6a5f4034a8697b4015
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-27 20:16:34 -0300

    fixx: v4 do exercicio de boa noite

--- algoritimosDeProgramacao/Aulas/27-08-2025/boaNoite.c
+++ algoritimosDeProgramacao/Aulas/27-08-2025/boaNoite.c
@@ -2,9 +2,18 @@
 
 int main(){
 
-    for(int i = 1; i <= 3 ; i++){
+    int qntBoaNoite, idadeUser;
+
+    printf("\nDigite quantos 'Boa noite' voce quer receber: \n");
+    scanf("%d", &qntBoaNoite);
+    printf("\nDigite a sua idade: \n");
+    scanf("%d", &idadeUser);
+
+
+    for(int i = 1; i <= qntBoaNoite ; i++){
         printf("\nBoa noite %d\n", i);
     }
+    printf("Sua idade eh %d", idadeUser);
 
 
     return 0;
