70e25725a41b26bbe8f1e5871ae1acf21c6af3f7
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-29 07:47:30 -0300

    addd: finalização do ex6

--- algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex6.c
+++ algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex6.c
@@ -2,5 +2,14 @@
 
 int main(){
 
+    int count = 1;
+    while(count <= 200){
+        if (count % 4 == 0 && count <= 200){
+            printf("\n%d eh um numero divisivel por 4 e menor que 200\n", count);
+        }
+        count++;
+    }
+
+
     return 0;
 }
