12e5bcc4151f0807daf9b2f3d395678cf3eb3c6e
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-29 08:01:29 -0300

    addd: começo ex8

--- algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex8.c
+++ algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex8.c
@@ -2,5 +2,24 @@
 
 int main(){
 
+    int quadradosXadrez = 64;
+    int count, graosTrigo, somaFinal;
+
+
+    count = 1;
+    graosTrigo = 1;
+    somaFinal = 0;
+
+    while(count <= quadradosXadrez){
+
+        printf("\n%d quadrado tem %d graos de trigo\n", quadradosXadrez, graosTrigo);
+        
+        somaFinal = somaFinal + graosTrigo;
+        graosTrigo = graosTrigo*2;
+        
+        count++;
+    }
+    printf("\nTemos %d graos de trigo no tabuleiro\n", graosTrigo);
+
     return 0;
 }
