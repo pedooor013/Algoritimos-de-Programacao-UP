3a015e1e0d33b1bf1f613c484f86147c5bb86a20
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-29 19:15:32 -0300

    fixx: Correção codigo de calculo de notas em massa

--- algoritimosDeProgramacao/Aulas/27-08-2025/exPrincipal.c
+++ algoritimosDeProgramacao/Aulas/27-08-2025/exPrincipal.c
@@ -18,9 +18,9 @@ int main(){
     int countAlunos = 0; 
     parar = 1;
     
-    while(parar != 0){
+    while(parar != 2){
         numBimestre = 1;
-        printf("\nDigite o nome da turma: \n");
+        printf("\nDigite a letra da turma: \n");
         scanf("%s", &nomeTurma);
         
         printf("\nDigite a quantidade de alunos: \n");
@@ -31,8 +31,9 @@ int main(){
         alunos = realloc(alunos, countAlunos * sizeof(struct Aluno));
 
 
-        for(int count = 0; count < qntAlunos; count++){
-
+        for(int count = 1; count <= qntAlunos; count++){
+            printf("\n===Notas aluno===\n");
+            //Calculo de notas dos 4 bimestres
             while(numBimestre <= 4){
                 printf("\nDigite a nota da prova do %d bimestre do aluno (0 a 8): \n", numBimestre);
                 scanf("%f", &notaProva);
@@ -53,42 +54,47 @@ int main(){
                     printf("\nDigite a nota do trabalho do %d bimestre do aluno (0 a 2): \n", numBimestre);
                     scanf("%f", &notaTrabalho);
                 }
-
+                
                 somaTotal = somaTotal + notaTrabalho + notaProva;
-
+                
                 numBimestre++;
             }
-            mediaAluno = somaTotal/4;
-            
-            if(mediaAluno >= 6.75){
-                alunos[count].turma = nomeTurma;
-                alunos[count].media = mediaAluno;
-                strcpy(alunos[count].statusAprovacao, "Aprovado!");
-            }else if(mediaAluno > 4 && mediaAluno < 6.75){
+
+                mediaAluno = somaTotal/4;
+                printf("\nA media do aluno foi de %.2f\n", mediaAluno);
                 
-                printf("\nNecessario fazer exame...\n");
-                printf("\nDigite a nota do exame: \n");
-                scanf("%f", &notaExame);
-                mediaAluno = (mediaAluno + notaExame) / 2;
-                
-                if(mediaAluno >= 5){
+                if(mediaAluno >= 6.75){
                     alunos[count].turma = nomeTurma;
                     alunos[count].media = mediaAluno;
-                    strcpy(alunos[count].statusAprovacao, "Aprovado com exame!");
-                }
-                else{
-                
+                    strcpy(alunos[count].statusAprovacao, "\nAprovado!\n");
+
+                }else if(mediaAluno > 4 && mediaAluno < 6.75){
+                    printf("\nNecessario fazer exame...\n");
+                    printf("\nDigite a nota do exame (0 a 10): \n");
+                    scanf("%f", &notaExame);
+                    mediaAluno = (mediaAluno + notaExame) / 2;
+                    
+                    if(mediaAluno >= 5){
+                        alunos[count].turma = nomeTurma;
+                        alunos[count].media = mediaAluno;
+                        strcpy(alunos[count].statusAprovacao, "Aprovado com exame!");
+                    }
+                    else{
+                        
+                        alunos[count].turma = nomeTurma;
+                        alunos[count].media = mediaAluno;
+                        strcpy(alunos[count].statusAprovacao, "Reprovado!");
+                        
+                    }
+                }else if(mediaAluno<4){
+                    
                     alunos[count].turma = nomeTurma;
                     alunos[count].media = mediaAluno;
                     strcpy(alunos[count].statusAprovacao, "Reprovado!");
-                    
-                }
-            }else if(mediaAluno<4){
-            
-                alunos[count].turma = nomeTurma;
-                alunos[count].media = mediaAluno;
-                strcpy(alunos[count].statusAprovacao, "Reprovado!");
-            };
+                };
+                numBimestre = 0;
+                mediaAluno = 0;
+                somaTotal = 0;
         };
         printf("\nDeseja calcular as notas de outras turmas (1-SIM, 2-NAO):\n");
         scanf("%d", &parar);
