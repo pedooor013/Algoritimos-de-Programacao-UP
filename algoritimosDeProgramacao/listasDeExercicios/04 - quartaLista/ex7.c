2a9c12356b28af872ec6c3d198ef77a0c6c9041c
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-29 07:55:02 -0300

    addd: finalização ex7

--- algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex7.c
+++ algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex7.c
@@ -2,5 +2,16 @@
 
 int main(){
 
+    float grausCelsius, grausFahrenheit;
+    
+    grausCelsius = 10;
+
+    while(grausCelsius <= 100){
+        grausFahrenheit = (grausCelsius * 9/5) + 32;
+
+        printf("\n%.2f graus celsius = %.2f graus fahrenheit\n", grausCelsius, grausFahrenheit);
+        grausCelsius = grausCelsius + 10;
+    }
+
     return 0;
 }
