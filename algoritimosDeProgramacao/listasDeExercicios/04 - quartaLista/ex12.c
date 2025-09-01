e63334795c8fc064ad449c6e6a005e4012fe1a32
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-01 16:50:03 -0300

    addd: resolução do ex12

--- algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex12.c
+++ algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex12.c
@@ -2,5 +2,19 @@
 
 int main(){
 
+    int mediaFinal,count, somaFinal, countNumPar;
+
+    somaFinal = 0;
+    count = 50;
+    countNumPar = 0;
+    do{
+        if (count%2 == 0){
+            somaFinal = somaFinal + count;
+            countNumPar++;
+        }
+        count++;
+    }while(count <= 70);
+    mediaFinal = somaFinal / countNumPar;
+    printf("\nMedia = %d\nSoma final = %d\n", mediaFinal, somaFinal);
     return 0;
 }
