d301ce7a4718fe5b999bace074482a2d30c528d9
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-01 17:07:49 -0300

    addd: resolução do ex13

--- algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex13.c
+++ algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex13.c
@@ -2,5 +2,30 @@
 
 int main(){
 
+    float larguraComodo, comprimentoComodo, metrosQuadradosTotal, metrosQuadradosComodo;
+    char respostaUser = 'S';
+    char nomeComodo[30];
+    
+    metrosQuadradosTotal = 0;
+    do{
+        printf("\nDigite o nome do comodo: \n");
+        scanf("%s", nomeComodo);
+
+        printf("\nDigite a largura do comodo: \n");
+        scanf("%f", &larguraComodo);
+        
+        printf("\nDigite a comprimento do comodo: \n");
+        scanf("%f", &comprimentoComodo);
+
+        metrosQuadradosComodo = comprimentoComodo * larguraComodo;
+        metrosQuadradosTotal = metrosQuadradosTotal + metrosQuadradosComodo;
+
+        printf("\nO seu comodo: %s tem %.2f metros quadrados\n\n\nVoce deseja calcular outros comodo (S = SIM; N = NAO):", nomeComodo, metrosQuadradosComodo);
+        scanf(" %c", &respostaUser);
+
+    }while(respostaUser != 'N');
+
+    printf("\nA sua residencia tem %.2f metros quadrados\n", metrosQuadradosTotal);
+
     return 0;
 }
