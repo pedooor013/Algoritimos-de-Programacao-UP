be9b4fabd00ddfe322a707d9cea848fe13f2ede5
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-01 19:03:12 -0300

    addd: resolulção ex14

--- algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex14.c
+++ algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex14.c
@@ -2,5 +2,25 @@
 
 int main(){
 
+    int numUser, maiorNum, menorNum, count;
+
+    count = 1;
+    do{
+        printf("\nDigite um numero: \n");
+        scanf("%d", &numUser);
+        if(count == 1){
+            maiorNum = numUser;
+            menorNum = numUser;
+        }
+        if(numUser > maiorNum){
+            maiorNum = numUser;
+        }else if(numUser < menorNum && numUser >= 0){
+            menorNum = numUser;
+        }
+        count++;
+    }while(numUser >= 0);
+
+    printf("\nO maior numero eh %d\nO menor numero eh %d\n", maiorNum, menorNum);
+
     return 0;
 }
