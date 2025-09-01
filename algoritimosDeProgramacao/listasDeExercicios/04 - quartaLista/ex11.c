345c19eb9de790cbd2ad72a09a659076070b4bcf
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-01 16:39:55 -0300

    addd: resolução do ex11

--- algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex11.c
+++ algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex11.c
@@ -2,5 +2,24 @@
 
 int main(){
 
+    int count, result, countFatorial;
+
+    result = 1;
+    count = 1;
+    do{
+        countFatorial = count;
+        result = count;
+        if(count%2 != 0){
+            while (countFatorial > 1){
+                printf("\nEsse eh o result = %d \n Esse eh o countFatorial = %d\n", result, countFatorial);
+                    result = result * (countFatorial-1);
+
+                countFatorial--;
+            }            
+            printf("\n!%d = %d\n", count, result);
+        }
+        count++;
+    }while(count <= 10);
+
     return 0;
 }
