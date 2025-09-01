e16379c09d764a05a50175acd1c2a9ae1ae3662e
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-31 23:00:34 -0300

    addd: resolução ex9

--- algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex9.c
+++ algoritimosDeProgramacao/listasDeExercicios/04 - quartaLista/ex9.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+
+
 int main(){
+    int soma, numUser, count;
+    float mediaNum;
+
+    count = 1;
+    soma = 0;
+    mediaNum = 0;
+    while (count <= 10){
+        
+        printf("\nDigite o valor do %d numero: \n", count);
+        scanf("%d", &numUser);
+
+        soma = numUser + soma;
+
+        count++;
+    }
+    
+    mediaNum = (float)soma / 10;
+
+    printf("\nA media dos 10 numeros eh igual a %.2f\nA soma dos 10 numero eh igual a %d\n", mediaNum, soma);
 
     return 0;
 }
