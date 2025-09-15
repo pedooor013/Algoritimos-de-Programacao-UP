4d65104803b63a7113a86225ae7e5411debaf275
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-15 15:25:05 -0300

    addd: comeco do ex6

---
+++ algoritimosDeProgramacao/listasDeExercicios/05 - quintaLista/ex6.c
@@ -0,0 +1,19 @@
+/* 
+06) Elaborar um programa que leia duas matrizes A e B, cada uma de duas dimensões com cinco linhas e três
+colunas para valores inteiros. Construir uma matriz C de mesma dimensão, que seja formada pela soma
+dos elementos da matriz A com os elementos da matriz B. Apresentar os elementos da matriz C.
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    int matrizA[5][3], matrizB[5][3], matrizC[5][3];
+    int contadorDeRepeticoesArray = 0;
+
+    for(){
+        
+    }
+
+    return 0;
+}
