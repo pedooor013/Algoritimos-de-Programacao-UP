b6c481b4c217f282b1af8e1070107094d6d9bf6e
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-15 19:43:15 -0300

    addd: finalização do ex6

--- algoritimosDeProgramacao/listasDeExercicios/05 - quintaLista/ex6.c
+++ algoritimosDeProgramacao/listasDeExercicios/05 - quintaLista/ex6.c
@@ -9,10 +9,28 @@ dos elementos da matriz A com os elementos da matriz B. Apresentar os elementos 
 int main(){
 
     int matrizA[5][3], matrizB[5][3], matrizC[5][3];
-    int contadorDeRepeticoesArray = 0;
+    int contadorDeRepeticoesArray = 0, count = 0;
 
-    for(){
-        
+    for(contadorDeRepeticoesArray = 0; contadorDeRepeticoesArray < 5; contadorDeRepeticoesArray++){
+        while(count < 3){
+            printf("\nDigite um numero: \n");
+            scanf("%d", &matrizA[contadorDeRepeticoesArray][count]);
+            printf("\nDigite um numero: \n");
+            scanf("%d", &matrizB[contadorDeRepeticoesArray][count]);
+
+            matrizC[contadorDeRepeticoesArray][count] = matrizA[contadorDeRepeticoesArray][count] + matrizB[contadorDeRepeticoesArray][count];  
+
+            count++;
+        }
+        count = 0;
+    }
+    for(contadorDeRepeticoesArray = 0; contadorDeRepeticoesArray < 5; contadorDeRepeticoesArray++){
+        while(count < 3){
+            printf("\t%d.%d - %d\t", contadorDeRepeticoesArray + 1, count + 1, matrizC[contadorDeRepeticoesArray][count]);
+            count++;
+        }
+        printf("\n");
+        count = 0;
     }
 
     return 0;
